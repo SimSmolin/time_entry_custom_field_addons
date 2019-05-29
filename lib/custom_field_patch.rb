60e70f262e7b0d893aad62fbe11b7e837a45be35
simsmolin <smolinsim@gmail.com>
2019-05-29 16:49:53 +0400

    Add correct probe for different typr CustomField

--- lib/custom_field_patch.rb
+++ lib/custom_field_patch.rb
@@ -14,21 +14,36 @@ module CustomFieldPatch
     def validate_custom_value_with_patch(custom_value)
       value = custom_value.value
       errs = format.validate_custom_value(custom_value)
-
-      # custom_value.custom_field.visible_by?(Project.find_by_id(custom_value.customized.project_id), User.current) - это видимость поля в форме
-      unless errs.any?
-        if value.is_a?(Array)
-          if !multiple?
-            errs << ::I18n.t('activerecord.errors.messages.invalid')
+      if custom_value.customized.is_a?(TimeEntry)
+        unless errs.any?
+          if value.is_a?(Array)
+            if !multiple?
+              errs << ::I18n.t('activerecord.errors.messages.invalid')
+            end
+            # if is_required? && value.detect(&:present?).nil?
+            if custom_value.custom_field.visible_by?(Project.find_by_id(custom_value.customized.project_id), User.current) && is_required? && value.detect(&:present?).nil?
+              errs << ::I18n.t('activerecord.errors.messages.blank')
+            end
+          else
+            # if is_required? && value.blank?
+            if custom_value.custom_field.visible_by?(Project.find_by_id(custom_value.customized.project_id), User.current) && is_required? && value.blank?
+              errs << ::I18n.t('activerecord.errors.messages.blank')
+            end
           end
-          # if is_required? && value.detect(&:present?).nil?
-          if custom_value.custom_field.visible_by?(Project.find_by_id(custom_value.customized.project_id), User.current) && is_required? && value.detect(&:present?).nil?
-            errs << ::I18n.t('activerecord.errors.messages.blank')
-          end
-        else
-          # if is_required? && value.blank?
-          if custom_value.custom_field.visible_by?(Project.find_by_id(custom_value.customized.project_id), User.current) && is_required? && value.blank?
-            errs << ::I18n.t('activerecord.errors.messages.blank')
+        end
+      else
+        unless errs.any?
+          if value.is_a?(Array)
+            if !multiple?
+              errs << ::I18n.t('activerecord.errors.messages.invalid')
+            end
+            if is_required? && value.detect(&:present?).nil?
+              errs << ::I18n.t('activerecord.errors.messages.blank')
+            end
+          else
+            if is_required? && value.blank?
+              errs << ::I18n.t('activerecord.errors.messages.blank')
+            end
           end
         end
       end
