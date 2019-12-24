a0247c34b598315ca94e01e47b8f390cdcfe13ac
sim <smolinsim@gmail.com>
2019-12-24 20:18:36 +0400

    новый вариант расчета даты

--- lib/time_entry_patch.rb
+++ lib/time_entry_patch.rb
@@ -74,13 +74,13 @@ module TimeEntryPatch
     end
 
     def valid_period_close?(date_for_field)
-      advantage_time = 0
+      close_day = Setting.plugin_time_entry_custom_field_addons['period_close_date'].to_i || 0
       if User.current.roles_for_project(project).reject { |role| !role.permissions.include?(:edit_time_entries_advantage_time) }.present?
-        advantage_time = Setting.plugin_time_entry_custom_field_addons['advantage_period'].to_i
+        close_day = Setting.plugin_time_entry_custom_field_addons['advantage_period_close_date'].to_i || 0
       end
       date_for_field = date_for_field || DateTime.parse('1970-01-01')
-      shift = Setting.plugin_time_entry_custom_field_addons['period_close_date'].to_i > (DateTime.now.day - advantage_time) ? 1:0
-      val_setting_months = Setting.plugin_time_entry_custom_field_addons['months_ago'].to_i + shift
+      currently_closed = close_day > DateTime.now.day ? 1:0
+      val_setting_months = Setting.plugin_time_entry_custom_field_addons['months_ago'].to_i + currently_closed
       date_close = DateTime.now.beginning_of_month - val_setting_months.month
       date_for_field < date_close
     end
